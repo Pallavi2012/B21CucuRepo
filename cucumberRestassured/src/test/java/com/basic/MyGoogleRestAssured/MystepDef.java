963ad68faa276fd66487e32d71c13ac8ad77d0a5
Admin <Admin@Admin-PC>
2020-02-29 11:22:11 +0530

    my 3 commit

--- cucumberRestassured/src/test/java/com/basic/MyGoogleRestAssured/MystepDef.java
+++ cucumberRestassured/src/test/java/com/basic/MyGoogleRestAssured/MystepDef.java
@@ -1,8 +1,10 @@
 package com.basic.MyGoogleRestAssured;
 
-	import org.testng.Assert;
+	
 
-	import cucumber.api.java.en.Given;
+	import org.junit.Assert;
+
+import cucumber.api.java.en.Given;
 	import cucumber.api.java.en.Then;
 	import cucumber.api.java.en.When;
 	import io.restassured.RestAssured;
